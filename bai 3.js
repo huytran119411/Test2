093e6f2251ee7f7c26e491917d53308423352ddd
huytran119411 <huytran119411@gmail.com>
2022-05-17 20:14:39 +0700

    Add file

--- bai 3.js
+++ bai 3.js
@@ -3,12 +3,17 @@ let array = new Array(n)
 for (let i = 0; i < array.length; i++) {
     array[i] = +prompt("Nhập giá trị của phần tử thứ " + (i + 1))
 }
-let k = 0
+let sum = 0;
+let count = 0;
 for (let x = 0; x < array.length; x++) {
-    for (let j = 2; j <= array[x]; j++) {
-        if (array[x] % j === 0) {
-            k = k + j
+        for (let j = 1; j <= array[x]; j++) {
+            if (array[x] % j === 0) {
+                count += 1;
+            }
         }
-    }
+        if (count === 2) {
+            sum += array[x]
+        }
+        count = 0;
 }
-document.write("Tổng của các số nguyên tố là: " + k)
+document.write("Tổng của các số nguyên tố là: " + sum)
